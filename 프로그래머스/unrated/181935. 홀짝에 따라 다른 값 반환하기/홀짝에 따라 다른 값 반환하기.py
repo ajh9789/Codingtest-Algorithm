d05786b8413aa29af43db71c09c4d43b0e7e1a71
ajh9789 <89918482+ajh9789@users.noreply.github.com>
2023-05-29 23:33:25 +0900

    [unrated] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.00 ms, Memory: 9.97 MB -BaekjoonHub

--- 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.py
+++ 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.py
@@ -13,3 +13,7 @@ def solution(n):
 # for i in n : 사용했을때
 #TypeError: 'int' object is not iterable 
 #list([iterable]) 여기서 iterable은 단일 객체가 아닌 반복할 수 있는 문자열, 튜플, 딕셔너리, range() 함수 등을 의미
+
+#참고할만한거 return sum(x ** (2 - x % 2) for x in range(n + 1) if n % 2 == x % 2)
+# if n % 2 == x % 2 이걸로 짝수일땐 짝수 홀수일땐 홀수 나뉘고
+#제곱 부분을 (2 - x % 2)로 처리해서 홀수 짝수 케이스나눔
