82a8c0c9c1463f3bb83f4466390d678cb0ad98f7
ajh9789 <89918482+ajh9789@users.noreply.github.com>
2023-05-30 17:59:23 +0900

    [unrated] Title: 조건 문자열, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.py
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.py
@@ -20,3 +20,5 @@ def solution(ineq, eq, n, m):
             return 1
         else:
             return 0
+# 참고 return int(eval(str(n)+ineq+eq.replace('!', '')+str(m)))
+#내장 함수 eval을 사용하면 문자열 형태의 코드를 실행할 수 있습니다.
