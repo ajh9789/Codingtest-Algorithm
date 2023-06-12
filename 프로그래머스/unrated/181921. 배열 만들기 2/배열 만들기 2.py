36743b4aba64a094822b19ee5accf2be34206497
ajh9789 <89918482+ajh9789@users.noreply.github.com>
2023-06-12 17:14:36 +0900

    [unrated] Title: 배열 만들기 2, Time: 98.57 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.py
@@ -11,3 +11,12 @@ def solution(l, r):
     if answer == [] :
         answer.append(-1)
     return answer
+
+#집합을 사용하면 더 최적화가능할듯!
+# set 사용한 다른사람 풀이
+# def solution(l, r):
+#     answer = []
+#     for num in range(l, r + 1):
+#         if not set(str(num)) - set(['0', '5']):
+#             answer.append(num)
+#     return answer if answer else [-1]
