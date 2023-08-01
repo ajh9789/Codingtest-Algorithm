323b634e219f7c45beea6d6dc39f02a2f1d3712c
ajh9789 <89918482+ajh9789@users.noreply.github.com>
2023-08-01 16:19:32 +0900

    [unrated] Title: 주사위 게임 3, Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181916. 주사위 게임 3/주사위 게임 3.py
+++ 프로그래머스/unrated/181916. 주사위 게임 3/주사위 게임 3.py
@@ -5,24 +5,24 @@ def solution(a, b, c, d):
     setnum = set(num)
     if len(setnum)==1: #모두가 같을 경우
         return 1111*num[0]
-    if len(setnum)==2: #1122나 1222 2221경우
+    if len(setnum)==2: # 2종류일 경우
         if num[1]==num[2]:
-            if(num[0]==num[1]):
+            if(num[0]==num[1]): #2221
                 return (10*num[0]+num[3])**2
-            else:
+            else: #1222
                 return (10*num[3]+num[0])**2
-        else:
+        else:  ##1122 
             return num[2]**2-num[1]**2
         
-    if len(setnum)==3 : #1123 1223 1233
-        if num[0]==num[1]:
+    if len(setnum)==3 :  #3종류일 경우
+        if num[0]==num[1]:#1123
             return num[2]*num[3]
-        elif num[1]==num[2]:
+        elif num[1]==num[2]: #1223
             return num[0]*num[3]
-        elif num[2]==num[3]:
+        elif num[2]==num[3]: #1233
             return num[0]*num[1]
         
-    if len(setnum)==4: #1234
+    if len(setnum)==4: #4종류일 경우 1234
         return num[0]
     
     #다른사람코드 각 숫자별 카운팅
