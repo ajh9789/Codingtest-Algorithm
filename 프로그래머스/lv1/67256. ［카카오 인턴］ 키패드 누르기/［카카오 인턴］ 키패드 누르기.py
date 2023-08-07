6d3e908e0314065a0a378fce2c93d02536cf747c
ajh9789 <89918482+ajh9789@users.noreply.github.com>
2023-08-07 14:55:32 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.89 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -52,3 +52,25 @@ def solution(numbers, hand):
                 left = number
                 result += "L"
     return result
+
+
+# 키보드 매핑방법1
+#     keypad = {
+#         1: (0, 0), 2: (1, 0), 3: (2, 0),
+#         4: (0, 1), 5: (1, 1), 6: (2, 1),
+#         7: (0, 2), 8: (1, 2), 9: (2, 2),
+#         0: (1, 3)
+#     }
+#     for number in numbers:
+#     number_coord = keypad[number]
+
+# 키보드 매핑방법2
+#     for number in numbers:
+#         # 각 숫자에 대한 좌표 설정
+#         number_coords = [
+#             [0, 0], [1, 0], [2, 0],
+#             [0, 1], [1, 1], [2, 1],
+#             [0, 2], [1, 2], [2, 2],
+#             [1, 3]  # 0은 (1, 3)에 위치
+#         ]
+#         number_coord = number_coords[number]
