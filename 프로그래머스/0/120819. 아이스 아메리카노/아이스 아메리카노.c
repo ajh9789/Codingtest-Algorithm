2b97301712905931e49e0886ad771e404a6fc9e2
ajh9789 <89918482+ajh9789@users.noreply.github.com>
2024-05-21 03:50:19 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int* solution(int money) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int* answer = (int*)malloc(sizeof(int)*2);
+    int result = money/5500;
+    int change = money%5500;
+    answer[0]=result;
+    answer[1]=change;
+    return answer;
+}
